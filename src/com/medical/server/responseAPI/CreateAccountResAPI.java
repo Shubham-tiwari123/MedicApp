7d195b0b7ecaef697d2a903beae9c1b484c61173
Shubham-tiwari123 <st540362@gmail.com>
2019-12-13 19:38:50 +0530

    Server Side

--- src/com/medical/server/responseAPI/CreateAccountResAPI.java
+++ src/com/medical/server/responseAPI/CreateAccountResAPI.java
@@ -1,24 +1,33 @@
 package com.medical.server.responseAPI;
 
+import org.json.simple.JSONObject;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 @WebServlet(name = "CreateAccountResAPI")
 public class CreateAccountResAPI extends HttpServlet {
-    static int patientId;
+    private static long patientId;
+    private static int responseCode;
 
-    public void sendResponse(int patientID,HttpServletResponse response)
-            throws ServletException, IOException {
-        patientId=patientID;
+    public void sendResponse(long patientID,int responseCode,HttpServletResponse response)
+            throws  IOException {
+        CreateAccountResAPI.patientId=patientID;
+        CreateAccountResAPI.responseCode=responseCode;
         doPost(response);
     }
 
-    protected void doPost(HttpServletResponse response)
-            throws ServletException, IOException {
-
+    private void doPost(HttpServletResponse response)
+            throws  IOException {
+        PrintWriter writer = response.getWriter();
+        JSONObject json = new JSONObject();
+        json.put("statusCode",responseCode);
+        json.put("patientId",patientId);
+        writer.println(json.toString());
     }
 }

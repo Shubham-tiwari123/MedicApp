79434f23cdc8cec27a574a2e8d6ba6db1e61cf92
Shubham-tiwari123 <st540362@gmail.com>
2019-12-14 19:28:22 +0530

    Server Side

--- src/com/medical/server/requestAPI/AppendBlockReqAPI.java
+++ src/com/medical/server/requestAPI/AppendBlockReqAPI.java
@@ -1,6 +1,7 @@
 package com.medical.server.requestAPI;
 
 import com.medical.server.entity.ClientSideBlock;
+import com.medical.server.entity.ClientSideBlockHash;
 import com.medical.server.entity.SetKeys;
 import com.medical.server.responseAPI.AppendBlockResAPI;
 import com.medical.server.service.AppendData;
@@ -10,24 +11,24 @@ import com.medical.server.utils.VariableClass;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 
-import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.security.NoSuchAlgorithmException;
 import java.sql.Date;
 import java.sql.Time;
 import java.time.LocalDate;
-import java.time.LocalTime;
 import java.util.ArrayList;
 
 @WebServlet(name = "AppendBlockReqAPI", urlPatterns = {"/appendRecord"})
 public class AppendBlockReqAPI extends HttpServlet {
 
-    private static ArrayList<byte[]> dataFromClient(long patientId){
-        ClientSideBlock block = new ClientSideBlock();
+    private static ArrayList<byte[]> dataFromClient(long patientId) throws NoSuchAlgorithmException {
+        ExtraFunctions extraFunctions = new ExtraFunctions();
+        ClientSideBlockHash block = new ClientSideBlockHash();
         block.setPatientId(patientId);
         block.setDate(Date.valueOf(LocalDate.now()));
         block.setTime(Time.valueOf("20:17:36"));
@@ -37,29 +38,34 @@ public class AppendBlockReqAPI extends HttpServlet {
         block.setPrescription("mlnfnlnoi  bdnigt gfifnjg rgieningr sirggr rgrgjgjogrgr geigjsofnfrirgj" +
                 "lngngrg gignrgrklvjrprjr rgipoojr pojwoif fofnln hpoojffjr goioirijfdnfgrirnwfpwfwlln" +
                 "sninfnvg ivoienreo  vsosd rlnfefnfw wrgrsnf efe gnacn ofn ubn; jfdkdureaw");
-        //block.setCurrentBlockHash("-10882-37-30-42-96-265669123-10843931101-77-119477105-78120-64-25-77-14-784824-3113-7");
-        block.setCurrentBlockHash("-10882-37-30-42-96-265669123-10843931101-77-119477105-78120-64-25-77-14-784824-3113-7");
-        String encryptString = "{\"patientId\":1234,\"date\":1576089000000,\"time\":\"20:17:36\",\"hospitalName\":\"medic center\"," +
-                "\"doctorName\":\"Mr Ram\",\"specialistType\":\"Surgeon\",\"prescription\":\"mlnfnlnoi  bdnigt gfifnjg rgieningr sirggr " +
-                "rgrgjgjogrgr geigjsofnfrirgjlngngrg gignrgrklvjrprjr rgipoojr pojwoif fofnln hpoojffjr goioirijfdnfgrirnwfpwfwllnsninfnvg " +
-                "ivoienreo  vsosd rlnfefnfw wrgrsnf efe gnacn ofn ubn; jfdkdureaw\",\"currentBlockHash\":\"-10882-37-30-42-96-265669123-108439" +
-                "31101-77-119477105-78120-64-25-77-14-784824-3113-7\"}";
+        System.out.println("block:"+extraFunctions.convertJavaToJson(block));
+        String calHash = extraFunctions.calculateHash(extraFunctions.convertJavaToJson(block));
 
+        ClientSideBlock sideBlock = new ClientSideBlock();
+        sideBlock.setPatientId(block.getPatientId());
+        sideBlock.setDate(block.getDate());
+        sideBlock.setTime(block.getTime());
+        sideBlock.setDoctorName(block.getDoctorName());
+        sideBlock.setHospitalName(block.getHospitalName());
+        sideBlock.setSpecialistType(block.getSpecialistType());
+        sideBlock.setPrescription(block.getPrescription());
+        sideBlock.setCurrentBlockHash(calHash);
+        String encryptString = extraFunctions.convertJavaToJson(sideBlock);
         System.out.println("en:"+encryptString);
         CreateAccount createAccount = new CreateAccount();
         ArrayList<byte[]> encryptedData = createAccount.encryptBlock(encryptString);
+        System.out.println("length:"+encryptedData.size());
         return encryptedData;
     }
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
+            throws IOException {
         AppendBlockResAPI resAPI = new AppendBlockResAPI();
         AppendData appendData = new AppendData();
         SetKeys keys = new SetKeys();
-        ExtraFunctions extraFunctions = new ExtraFunctions();
         StringBuilder buffer = new StringBuilder();
         BufferedReader reader = request.getReader();
-
+        int statusCode;
         String line;
         while((line = reader.readLine())!= null) {
             buffer.append(line);
@@ -72,7 +78,7 @@ public class AppendBlockReqAPI extends HttpServlet {
             long patientId = (Long) jSONObject.get("patientId");
             //ArrayList<String> encryptedValues = (ArrayList<String>) jSONObject.get("encryptedValues");
             System.out.println("id:-"+patientId);
-            ArrayList<byte[]> encryptedData = dataFromClient(1234);
+            ArrayList<byte[]> encryptedData = dataFromClient(patientId);
 
             //Start with server function
             if(appendData.verifyID(patientId)){
@@ -86,24 +92,34 @@ public class AppendBlockReqAPI extends HttpServlet {
                     String updatedBlock = appendData.updateBlock(lastBlockHash,decryptString);
                     System.out.println("updatedblock:\n"+updatedBlock);
 
-                    if(appendData.appendBlockInChain(patientId,updatedBlock,keys))
+                    if(appendData.appendBlockInChain(patientId,updatedBlock,keys)) {
+                        statusCode = VariableClass.SUCCESSFUL;
                         System.out.println("data saved");
-                    else
+                    }
+                    else {
+                        statusCode=VariableClass.FAILED;
                         System.out.println("not saved");
+                    }
                 }
-                else
+                else{
+                    statusCode= VariableClass.BAD_REQUEST;
                     System.out.println("not equal");
+                }
             }
-            else
+            else {
+                statusCode = VariableClass.BAD_REQUEST;
                 System.out.println("no record");
-            
+            }
+            resAPI.setStatusCode(statusCode,response);
         } catch (Exception e) {
             e.printStackTrace();
+            resAPI.setStatusCode(VariableClass.BAD_REQUEST,response);
         }
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-
+            throws IOException {
+        AppendBlockResAPI resAPI = new AppendBlockResAPI();
+        resAPI.setStatusCode(VariableClass.BAD_REQUEST,response);
     }
 }
--- src/com/medical/server/requestAPI/SendChainReqAPI.java
+++ src/com/medical/server/requestAPI/SendChainReqAPI.java
@@ -3,6 +3,8 @@ package com.medical.server.requestAPI;
 import com.medical.server.entity.SetKeys;
 import com.medical.server.responseAPI.SendChainResAPI;
 import com.medical.server.service.SendData;
+import com.medical.server.utils.VariableClass;
+import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 
 import javax.servlet.ServletException;
@@ -12,7 +14,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.BufferedReader;
 import java.io.IOException;
-import java.util.LinkedList;
+import java.util.ArrayList;
 import java.util.List;
 
 @WebServlet(name = "SendChainReqAPI", urlPatterns = {"/getChain"})
@@ -21,6 +23,7 @@ public class SendChainReqAPI extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         // read patientID from request
+
         SendChainResAPI resAPI = new SendChainResAPI();
         SendData sendData = new SendData();
         StringBuilder buffer = new StringBuilder();
@@ -32,29 +35,37 @@ public class SendChainReqAPI extends HttpServlet {
         }
         String data = buffer.toString();
         JSONParser parser = new JSONParser();
-        List<byte[]> encryptedData = new LinkedList<byte[]>();
+        ArrayList<ArrayList<byte[]>> encryptedData = null;
+
         try {
-            /* Parse the data to get patientID and encrypted data
             JSONObject jSONObject = (JSONObject) parser.parse(data);
-            ArrayList<String> encryptedValues = (ArrayList<String>) jSONObject.get("encryptedValues");
-            */
-            int patientId = 12345;
-            int hospitalId = 34567;
+            long patientId = (long) jSONObject.get("patientId");
+            //long hospitalId = (long) jSONObject.get("hospitalId");
             if(sendData.verifyID(patientId)){
+                System.out.println("user verified");
                 List<String> list = sendData.getDataDB(patientId);
-                SetKeys keys = sendData.getKeysOfClient(hospitalId);
-                encryptedData= sendData.encryptDataAgain(keys,list);
-                resAPI.sendResponse(encryptedData,response);
+                System.out.println("list size api:"+list.size());
+                // replace this
+                SetKeys keys = sendData.getKeysOfClient(1234);
+                // encrypt the data using client public key and send the data to client;
+                encryptedData = sendData.encryptDataAgain(keys,list);
+                for(ArrayList<byte[]> val:encryptedData){
+                    System.out.println(val);
+                }
+                resAPI.sendResponse(encryptedData,response,VariableClass.SUCCESSFUL);
             }
-            else
-                resAPI.sendResponse(encryptedData,response);
+            else{
+                resAPI.sendResponse(null,response,VariableClass.BAD_REQUEST);
+            }
         }catch (Exception e){
             System.out.println(e);
+            resAPI.sendResponse(null,response,VariableClass.FAILED);
         }
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-
+        SendChainResAPI resAPI = new SendChainResAPI();
+        resAPI.sendResponse(null,response,VariableClass.BAD_REQUEST);
     }
 }

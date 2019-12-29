20deb4a72bf8f19af86955acffe6c2127883a4d0
Shubham-tiwari123 <st540362@gmail.com>
2019-12-29 21:19:10 +0530

    Socket Pro for exchanging keys

---
+++ socket/SocketClient/com/project/ExtraFunctions.java
@@ -0,0 +1,83 @@
+package com.project;
+
+import javax.crypto.Cipher;
+import java.math.BigInteger;
+import java.nio.charset.StandardCharsets;
+import java.security.*;
+import java.security.spec.RSAPrivateKeySpec;
+import java.security.spec.RSAPublicKeySpec;
+import java.util.ArrayList;
+
+public class ExtraFunctions{
+
+    public String calculateHash(String value) throws Exception {
+        MessageDigest digest = MessageDigest.getInstance("SHA-256");
+        byte[] encodedHashValue = digest.digest(value.getBytes(StandardCharsets.UTF_8));
+        StringBuilder hashValue = new StringBuilder(2 * encodedHashValue.length);
+        for (byte b : encodedHashValue) {
+            hashValue.append(b);
+        }
+        return hashValue.toString();
+    }
+
+    public byte[] encryptData(String data, BigInteger modulus, BigInteger expo) throws Exception{
+        byte[] dataToEncrypt = data.getBytes(StandardCharsets.UTF_8);
+        byte[] encryptedData = null;
+            RSAPublicKeySpec rsaPublicKeySpec = new RSAPublicKeySpec(modulus, expo);
+            KeyFactory factory = KeyFactory.getInstance("RSA");
+            PublicKey publicKey = factory.generatePublic(rsaPublicKeySpec);
+            Cipher cipher = Cipher.getInstance("RSA");
+            cipher.init(Cipher.ENCRYPT_MODE, publicKey);
+            encryptedData = cipher.doFinal(dataToEncrypt);
+        return encryptedData;
+    }
+
+    public String decryptData(byte[] data, BigInteger modulus, BigInteger expo) throws Exception{
+        byte[] decryptedData;
+        String value = null;
+            RSAPrivateKeySpec rsaPrivateKeySpec = new RSAPrivateKeySpec(modulus, expo);
+            KeyFactory factory = KeyFactory.getInstance("RSA");
+            PrivateKey privateKey = factory.generatePrivate(rsaPrivateKeySpec);
+            Cipher cipher = Cipher.getInstance("RSA");
+            cipher.init(Cipher.DECRYPT_MODE, privateKey);
+            decryptedData = cipher.doFinal(data);
+            value = new String(decryptedData,StandardCharsets.UTF_8);
+        return value;
+    }
+
+    public String convertEncryptedData(ArrayList<byte[]> data, SetKeys getKeys) throws Exception{
+        StringBuilder builder = new StringBuilder();
+        for (byte[] byteValue : data) {
+            String val = decryptData(byteValue, getKeys.getPublicKeyModules(),
+                    getKeys.getPublicKeyExpo());
+            builder.append(val);
+        }
+        return builder.toString();
+    }
+
+    public <T> T convertJsonToJava(String jsonString, Class<T> obj) throws Exception{
+        return JSONUtil.convertJsonToJava(jsonString,obj);
+    }
+
+    public String convertJavaToJson(Object object) throws Exception{
+        return JSONUtil.convertJavaToJson(object);
+    }
+
+}
+/*
++447459165703
+ +447599495019
+ 8939183474
+ 7878260183
+ 8618984805
+ 8420069188
+ v07431834909 - L
+ v08436528564 - L
+ 08327891975
+ 09892855419
+ 07431841623
+
+ 08327615109 - college
+
+ 08536031692-41
+ */

d396057a636c7c42108380f80c38fbc1a383b507
Shubham-tiwari123 <st540362@gmail.com>
2020-02-12 21:53:13 +0530

    client API

--- src/com/medical/client/dao/Database.java
+++ src/com/medical/client/dao/Database.java
@@ -1,13 +1,13 @@
 package com.medical.client.dao;
 
-import com.medical.client.entity.GetKeys;
+import com.medical.client.entity.ClientKeys;
 import com.medical.client.entity.ServerKeys;
-import com.medical.client.entity.SetKeys;
 import com.medical.client.utils.VariableClass;
 import com.mongodb.MongoClient;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
 import com.mongodb.client.MongoIterable;
+import com.mongodb.client.result.DeleteResult;
 import org.bson.Document;
 
 import java.math.BigInteger;
@@ -53,23 +53,26 @@ public class Database implements DatabaseInterface {
         ServerKeys keys = new ServerKeys();
         if (createDbConn()) {
             if (checkCollection(collectionName)) {
-                MongoCollection<Document> collection = database.getCollection(collectionName);
+                MongoCollection collection = database.getCollection(collectionName);
                 System.out.println("getting keys from db");
-                List<Document> list = collection.find().into(new ArrayList<Document>());
+                List<Document> list = (List<Document>) collection.find(new Document("serverKeys",
+                        "Server Keys")).into(new ArrayList<Document>());
                 System.out.println("list:" + list.size());
 
                 if (!list.isEmpty()) {
                     for (Document val : list) {
                         System.out.println("getting server keys if");
-                        String publicKeyModules = val.getString("serverMod");
-                        String publicKeyExpo = val.getString("serverExpo");
+                        String publicKeyModules = val.getString("serverPubMod");
+                        String publicKeyExpo = val.getString("serverPubExpo");
 
                         System.out.println("setting server keys");
                         keys.setPublicKeyExpo(new BigInteger(publicKeyExpo));
                         keys.setPublicKeyModules(new BigInteger(publicKeyModules));
                     }
+                    client.close();
                     return keys;
                 } else {
+                    client.close();
                     return null;
                 }
 
@@ -78,7 +81,7 @@ public class Database implements DatabaseInterface {
         return null;
     }
 
-    @Override
+    /*@Override
     public boolean storeKeys(GetKeys keys, String collectionName, SetKeys setKeys) throws Exception {
         if (createDbConn()) {
             if (checkCollection(collectionName)) {
@@ -95,9 +98,9 @@ public class Database implements DatabaseInterface {
             return false;
         }
         return false;
-    }
+    }*/
 
-    @Override
+    /*@Override
     public SetKeys getClientKeys(String collectionName) throws Exception {
         SetKeys keys = new SetKeys();
         if (createDbConn()) {
@@ -124,6 +127,106 @@ public class Database implements DatabaseInterface {
             return null;
         }
         return null;
+    }*/
+
+    @Override
+    public boolean storeServerKeys(ServerKeys keys, String collectionName) throws Exception {
+        if (createDbConn()) {
+            if (checkCollection(collectionName)) {
+                System.out.println("saving client and server keys in db");
+                Document document = new Document("serverKeys", "Server Keys")
+                        .append("serverPubMod", keys.getPublicKeyModules().toString())
+                        .append("serverPubExpo", keys.getPublicKeyExpo().toString());
+                database.getCollection(collectionName).insertOne(document);
+                return true;
+            }
+            return false;
+        }
+        return false;
+    }
+
+    @Override
+    public boolean storeClientKeys(ClientKeys keys, String collectionName) throws Exception {
+        if (createDbConn()) {
+            if (checkCollection(collectionName)) {
+                System.out.println("saving client and server keys in db");
+                Document document = new Document("clientKeys","Client Keys")
+                        .append("clientPubMod", keys.getPublicKeyModules().toString())
+                        .append("clientPubExpo", keys.getPublicKeyExpo().toString())
+                        .append("clientPriExpo", keys.getPrivateKeyExpo().toString())
+                        .append("clientPriMod", keys.getPrivateKeyModules().toString());
+                database.getCollection(collectionName).insertOne(document);
+                return true;
+            }
+            return false;
+        }
+        return false;
+    }
+
+    @Override
+    public ClientKeys getClientKeys2(String collectionName) throws Exception {
+        ClientKeys keys = new ClientKeys();
+        if (createDbConn()) {
+            if (checkCollection(collectionName)) {
+                MongoCollection collection = database.getCollection(collectionName);
+                System.out.println("getting keys from db");
+                List<Document> list = (List<Document>) collection.find(new Document("clientKeys",
+                        "Client Keys")).into(new ArrayList<Document>());
+                System.out.println("list:" + list.size());
+
+                if (!list.isEmpty()) {
+                    for (Document val : list) {
+                        System.out.println("getting client keys ");
+                        String publicKeyModules = val.getString("clientPubMod");
+                        String publicKeyExpo = val.getString("clientPubExpo");
+                        String privateKeyModules = val.getString("clientPriMod");
+                        String privateKeyExpo = val.getString("clientPriExpo");
+
+                        System.out.println("setting client keys");
+                        keys.setPublicKeyExpo(new BigInteger(publicKeyExpo));
+                        keys.setPublicKeyModules(new BigInteger(publicKeyModules));
+                        keys.setPrivateKeyExpo(new BigInteger(privateKeyExpo));
+                        keys.setPrivateKeyModules(new BigInteger(privateKeyModules));
+                    }
+                    return keys;
+                }
+                return null;
+            }
+            return null;
+        }
+        return null;
+    }
+
+    @Override
+    public boolean deleteServerKeys(String collectionName) throws Exception {
+        if (createDbConn()) {
+            if (checkCollection(collectionName)) {
+                System.out.println("deleting database");
+                MongoCollection collection = database.getCollection(collectionName);
+                DeleteResult result = collection.deleteOne(new Document("serverKeys", "Server Keys"));
+                return result.getDeletedCount() != 0;
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public boolean checkKeysExists(String collectionName) throws Exception {
+        if(createDbConn()){
+            if(checkCollection(collectionName)){
+                MongoCollection collection = database.getCollection(collectionName);
+                System.out.println("getting keys from db");
+                List<Document> list = (List<Document>) collection.find(new Document("clientKeys",
+                        "Client Keys")).into(new ArrayList<Document>());
+
+                List<Document> list2 = (List<Document>) collection.find(new Document("serverKeys",
+                        "Server Keys")).into(new ArrayList<Document>());
+
+                return list.size() == 1 && list2.size() == 1;
+            }
+            return false;
+        }
+        return false;
     }
 
 }
--- src/com/medical/client/dao/DatabaseInterface.java
+++ src/com/medical/client/dao/DatabaseInterface.java
@@ -1,8 +1,7 @@
 package com.medical.client.dao;
 
-import com.medical.client.entity.GetKeys;
+import com.medical.client.entity.ClientKeys;
 import com.medical.client.entity.ServerKeys;
-import com.medical.client.entity.SetKeys;
 
 public interface DatabaseInterface {
     boolean createDbConn() throws Exception;
@@ -11,7 +10,18 @@ public interface DatabaseInterface {
 
     ServerKeys getServerKeys(String collectionName) throws Exception;
 
-    boolean storeKeys(GetKeys keys, String collectionName, SetKeys setKeys) throws Exception;
+    /*boolean storeKeys(GetKeys keys, String collectionName, SetKeys setKeys) throws Exception;*/
 
-    SetKeys getClientKeys(String collectionName) throws Exception;
+    /*SetKeys getClientKeys(String collectionName) throws Exception;*/
+
+    boolean storeServerKeys(ServerKeys keys, String collectionName) throws Exception;
+
+    boolean storeClientKeys(ClientKeys keys, String collectionName) throws Exception;
+
+    ClientKeys getClientKeys2(String collectionName) throws Exception;
+
+    boolean deleteServerKeys(String collectionName) throws Exception;
+
+    boolean checkKeysExists(String collectionName) throws Exception;
+
 }
